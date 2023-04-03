2ef3f0e6fd48cbf0bd2c08b4a0da72463e9e3a4b
wemersonlucas <w.lucas_sp_19@hotmail.com>
2023-04-02 22:15:42 -0300

    feat: Iniciando Todo-list

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState}from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableWithoutFeedback} from 'react-native';
+import { FlatList } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [tarefas, setTarefas] = useState([]);	
+
+  const handleAdd = () => {
+    setTarefas([...tarefas, text]);
+    setText('');
+  }
+
+  const handleRenderTarefas = ({item}) => {
+    return (
+      <Text>{item}</Text>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>App todo list</Text>
-      <StatusBar style="auto" />
-    </View>
+   <SafeAreaView style={styles.container}>
+      <Text>Todo list</Text>
+      <TextInput  
+      value={text}
+      onChangeText={text => setText(text)}
+        style={{
+          borderWidth: 1, 
+          borderColor: 'black', 
+          width: 200, height: 40
+      }}/>
+      <TouchableWithoutFeedback 
+        onPress={handleAdd}>
+        <Text style={{
+          borderRadius: 5,
+          borderColor: 'black',
+          width: 200,
+          height: 40,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 10,
+          backgroundColor: "aqua"
+        }}>Enviar</Text>
+      </TouchableWithoutFeedback>
+      <FlatList 
+      data={tarefas} 
+      keyExtractor={item => item}
+      renderItem={handleRenderTarefas}/>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +55,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
 });
